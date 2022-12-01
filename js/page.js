5e6074810e9287cd1615f0b47ea70676c14a28a0
Jamal <wilkyn.sd@eduge.ch>
2022-12-01 16:11:14 +0100

    Reorganisation des fichers

---
+++ js/page.js
@@ -0,0 +1,43 @@
+function ouvrirpage() {
+    var a = document.getElementById("search").value;
+
+    if (a === "Assassin's Creed Syndicate") {
+        window.location.href=("/ascSydycate.html")
+    }
+
+    if (a === "Forza Horizon 5") {
+        window.location.href=("/FH5.html")
+    }
+
+    if (a === "Fifa 23") {
+        window.location.href=("/fifa23.html")
+    }
+
+    if (a === "Garry's Mod") {
+        window.location.href=("/Garry's.html")
+    }
+
+    if (a === "NBA 2K23") {
+        window.location.href=("/NBA2K23.html")
+    }
+
+    if (a === "Need For Speed Heat") {
+        window.location.href=("/nfpHeat.html")
+    }
+
+    if (a === "PC Building Simulator 2") {
+        window.location.href=("/PcBuilding.html")
+    }
+
+    if (a === "Rainbow Six Siege") {
+        window.location.href=("/R6.html")
+    }
+
+    if (a === "Raft") {
+        window.location.href=("/Raft.html")
+    }
+
+    if (a === "The Forest") {
+        window.location.href=("/theForest.html")
+    }
+}
