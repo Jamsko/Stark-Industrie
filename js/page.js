4d9fbbee5186a9080f43fe00fbed2cb4ca897f7c
Jamal <wilkyn.sd@eduge.ch>
2022-12-19 08:29:51 +0100

    Amélioration de la barre de recherche

--- js/page.js
+++ js/page.js
@@ -1,43 +1,43 @@
 function ouvrirpage() {
     var a = document.getElementById("search").value;
 
-    if (a === "Assassin's Creed Syndicate") {
+    if (a.toLowerCase() === "assassin's creed syndicate") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/ascSydycate.html")
     }
 
-    if (a === "Forza Horizon 5") {
+    if (a.toLowerCase() === "forza horizon 5") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/FH5.html")
     }
 
-    if (a === "Fifa 23") {
+    if (a.toLowerCase() === "fifa 23") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/fifa23.html")
     }
 
-    if (a === "Garry's Mod") {
+    if (a.toLowerCase() === "garry's mod") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/Garry's.html")
     }
 
-    if (a === "NBA 2K23") {
+    if (a.toLowerCase() === "nba 2k23") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/NBA2K23.html")
     }
 
-    if (a === "Need For Speed Heat") {
+    if (a.toLowerCase() === "need for speed heat") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/nfpHeat.html")
     }
 
-    if (a === "PC Building Simulator 2") {
+    if (a.toLowerCase() === "pc building simulator 2") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/PcBuilding.html")
     }
 
-    if (a === "Rainbow Six Siege") {
+    if (a.toLowerCase() === "rainbow six siege") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/R6.html")
     }
 
-    if (a === "Raft") {
+    if (a.toLowerCase() === "raft") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/Raft.html")
     }
 
-    if (a === "The Forest") {
+    if (a.toLowerCase() === "the forest") {
         window.location.pathname=("/Atelier_Scrum/StarkGameStore/theForest.html")
     }
 }
